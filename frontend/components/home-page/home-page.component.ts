7cfc489f65deebe17d872d39cc50ad67c34c617d
bozhidarak <bgkbgk6@gmail.com>
2023-12-14 15:22:33 +0200

    button fix
    Using mat-button

--- frontend/components/home-page/home-page.component.ts
+++ frontend/components/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [CommonModule, NavBarComponent],
+  imports: [CommonModule, NavBarComponent, MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
