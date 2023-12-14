4895d4b4d4421222b81181d83b5c780b6593f452
bozhidarak <bgkbgk6@gmail.com>
2023-12-14 17:33:44 +0200

    added grid to display cards

--- frontend/components/pet-sitters-page/pet-sitters-page.component.ts
+++ frontend/components/pet-sitters-page/pet-sitters-page.component.ts
@@ -3,14 +3,30 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { MatButtonModule } from '@angular/material/button';
 import { PetCardComponent } from '../pet-card/pet-card.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'pet-sitters',
   standalone: true,
-  imports: [CommonModule, NavBarComponent,MatButtonModule, PetCardComponent],
+  imports: [CommonModule, NavBarComponent,MatButtonModule, PetCardComponent,MatGridListModule],
   templateUrl: './pet-sitters-page.component.html',
   styleUrl: './pet-sitters-page.component.css'
 })
 export class PetSittersPageComponent {
-
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 2, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  ];
 }
