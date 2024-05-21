b78784e33e9013605803893c0cc48e66b3420b6e
Yoana.Aneva <ioana.aneva@gmail.com>
2024-05-21 11:48:40 +0300

    Add DTO constraints

--- backend/src/main/java/com/example/backend/dto/PetOwnerOfferDTO.java
+++ backend/src/main/java/com/example/backend/dto/PetOwnerOfferDTO.java
@@ -1,5 +1,7 @@
 package com.example.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,8 +9,12 @@ import java.time.LocalDate;
 @Data
 public class PetOwnerOfferDTO {
     private Long id;
+    @NotBlank(message = "Location of PetOwnerOffer cannot be blank")
     private String location;
+    @NotNull(message = "Start date of PetOwnerOffer is required")
     private LocalDate startDate;
+    @NotNull(message = "End date of PetOwnerOffer is required")
     private LocalDate endDate;
+    @NotNull(message = "User ID of PetOwnerOffer is required")
     private Long userId;
 }
--- backend/src/main/java/com/example/backend/dto/ReviewDTO.java
+++ backend/src/main/java/com/example/backend/dto/ReviewDTO.java
@@ -1,11 +1,19 @@
 package com.example.backend.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 import lombok.Data;
 
 @Data
 public class ReviewDTO {
+    @NotNull(message = "Stars of Review are required")
+    @Min(value = 1, message = "Stars of Review must be min 1")
+    @Max(value = 5, message = "Stars of Review must be max 5")
     private Integer stars;
     private String description;
+    @NotNull(message = "The ID of Reviewed User is required")
     private Long reviewedUserId;
+    @NotNull(message = "The ID of Review Author is required")
     private Long authorId;
 }
