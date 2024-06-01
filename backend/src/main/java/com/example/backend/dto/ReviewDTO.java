b1359777d54459e87ae566da6f825026d9f0e788
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-01 17:42:31 +0300

    Add toString method

--- backend/src/main/java/com/example/backend/dto/ReviewDTO.java
+++ backend/src/main/java/com/example/backend/dto/ReviewDTO.java
@@ -4,9 +4,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class ReviewDTO {
+    private Long id;
     @NotNull(message = "Stars of Review are required")
     @Min(value = 1, message = "Stars of Review must be min 1")
     @Max(value = 5, message = "Stars of Review must be max 5")
--- backend/src/main/java/com/example/backend/entity/Review.java
+++ backend/src/main/java/com/example/backend/entity/Review.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Table(name="review")
 public class Review {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer stars;
     private String description;
@@ -19,4 +19,14 @@ public class Review {
     @JoinColumn(name="author_id")
     private User author;
     public Review(){}
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", stars=" + stars +
+                ", description='" + description + '\'' +
+                ", reviewedUser=" + reviewedUser.getId() +
+                ", author=" + author.getId() +
+                '}';
+    }
 }
