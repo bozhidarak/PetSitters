6c1ab565ae55368fd84fec56463fa5654b753454
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-04 12:44:05 +0300

    Add method for filtering offers by pet types

--- backend/src/main/java/com/example/backend/repository/PetOwnerOfferRepository.java
+++ backend/src/main/java/com/example/backend/repository/PetOwnerOfferRepository.java
@@ -1,12 +1,16 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.PetOwnerOffer;
+import com.example.backend.enums.PetType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PetOwnerOfferRepository extends JpaRepository<PetOwnerOffer, Long> {
     Page<PetOwnerOffer> findAll(Pageable pageable);
+    List<PetOwnerOffer> findByPetsPetTypeIn(List<PetType> petTypes);
 }
