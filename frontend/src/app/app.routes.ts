675ae8d9fefda6789245732c415c93538ef57326
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-12 13:59:51 +0300

    Change route to owner offer details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from '../../components/home-page/home-page.component';
 import { PetSittersPageComponent } from '../../components/pet-sitters-page/pet-sitters-page.component';
-import { OwnersPageComponent } from '../../components/owners-page/owners-page.component';
+import { OwnerOffersPageComponent } from '../../components/owner-offers-page/owner-offers-page.component';
 import { RegistrationComponent } from '../../components/registration/registration.component';
 import { LoginComponent } from '../../components/login/login.component';
 import { HowPageComponent } from '../../components/how-page/how-page.component';
 import { SitterDetailsComponent } from '../../components/sitter-details/sitter-details.component';
-import { OwnerDetailsComponent } from '../../components/owner-details/owner-details.component';
+import { OwnerOfferDetailsComponent } from '../../components/owner-offer-details/owner-offer-details.component';
 import { EditProfileComponent } from '../../components/edit-profile/edit-profile.component';
 
 export const routes: Routes = [
@@ -15,13 +15,11 @@ export const routes: Routes = [
     },
     {path:'home-page', component:HomePageComponent},
     {path:'pet-sitters', component:PetSittersPageComponent},
-    {path:'owners-page', component:OwnersPageComponent},
+    {path:'owners-page', component:OwnerOffersPageComponent},
     {path:'registration', component:RegistrationComponent},
     {path:'login', component:LoginComponent},
     {path: 'how-page', component:HowPageComponent},
     {path: 'sitter-details/:sitterEmail/:sitterEmailProvider/:sitterEmailDomain', component:SitterDetailsComponent},
-    {path: 'owner-details/:ownerEmail/:ownerEmailProvider/:ownerEmailDomain', component:OwnerDetailsComponent},
+    {path: 'owner-details/:id', component:OwnerOfferDetailsComponent},
     {path: 'edit-profile', component:EditProfileComponent}
-
-
 ];
