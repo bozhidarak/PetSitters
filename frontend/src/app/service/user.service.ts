7ecb161db47f7eb840d5730c24b2b730aeffffbc
bozhidarak <bgkbgk6@gmail.com>
2024-06-18 17:55:08 +0300

    created user service and fixed user model

---
+++ frontend/src/app/service/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/new-user-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUserById(id: number) {
+    return this.http.get(`api/users/${id}`);
+  }
+
+  login(email: string, password: string): Observable<any>{
+    return this.http.post(`api/users/login`, {email, password});
+  }
+
+  register(user: User, profilePic: File): Observable<User> {
+    console.log(user);
+    const formData = new FormData();
+
+    formData.append('userDTO', new Blob([JSON.stringify(user)], {type: 'application/json'}));
+    formData.append('profilePic', profilePic, profilePic.name);
+    
+    formData.forEach((value, key) => {console.log(key + ' ' + value)});
+
+    return this.http.post<User>(`api/users/register`, formData);
+  }
+
+}
--- frontend/src/models/new-user-model.ts
+++ frontend/src/models/new-user-model.ts
@@ -1,24 +1,15 @@
 export class User {
-    id: number;
-    username: string;
+    id?: number;
     password: string;
     email:string;
     location:string;
-    birthdate:Date;
-    gender:boolean;
-    profilePic:string;
     name:string;
 
 
-    constructor(id:number, username: string, password: string, email:string, location:string, birthdate:Date, gender:boolean, profilePic:string, name:string) {
-        this.id = id;
-        this.username = username;
+    constructor( password: string, email:string, location:string, name:string) {
         this.password = password;
         this.email = email;
         this.location = location;
-        this.birthdate = birthdate;
-        this.gender = gender;
-        this.profilePic = profilePic;
         this.name = name;
     }
 }
