50ae98ca716ae2f33d4a5df34af7f50d2798d3d0
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-20 16:11:33 +0300

    Validate review

--- frontend/components/create-review/create-review.component.ts
+++ frontend/components/create-review/create-review.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
-  selector: 'app-create-review',
+  selector: 'create-review',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgForOf,
+    MatButtonModule,
+    NgIf
+  ],
   templateUrl: './create-review.component.html',
   styleUrl: './create-review.component.css'
 })
 export class CreateReviewComponent {
+  description = new FormControl('', [Validators.required, Validators.minLength(2)]);
+  stars: number = 0;
+  isSubmitPressed: boolean = false;
 
+  setStars(value: number) {
+    this.stars = value;
+  }
+
+  handleReviewCreation() {
+    console.log(this.stars)
+    this.isSubmitPressed = true;
+    if(this.stars < 1 || !this.description) {
+      return
+    }
+
+  }
 }
