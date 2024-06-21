5a6e5b7631eb089a58be84f5f525692107882bd9
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-21 19:49:44 +0300

    Ajust filters

--- frontend/components/filters/filters.component.ts
+++ frontend/components/filters/filters.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component, Input} from '@angular/core';
+import {FormGroup, FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'filters',
   standalone: true,
-  imports: [MatCheckboxModule, FormsModule],
+  imports: [ReactiveFormsModule, MatCheckboxModule, FormsModule, MatButtonModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, NgIf],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
-export class FiltersComponent {
 
+export class FiltersComponent {
+  filterForm: FormGroup = this.formBuilder.group({
+    startDate: [Date],
+    endDate: [Date],
+  });
+  pets: string[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  addPetToFilters(pet: string) {
+    this.pets.push(pet);
+  }
 }
