7010d358d0a30c9f751d81eeccb16067a03248da
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-21 23:54:31 +0300

    Emit filters to offer pages

--- frontend/components/filters/filters.component.ts
+++ frontend/components/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormGroup, FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
@@ -16,9 +16,12 @@ import {NgIf} from "@angular/common";
 })
 
 export class FiltersComponent {
+  @Input() isSitterOffer: boolean = false;
+  @Output() filters = new EventEmitter<{pets: string[], startDate: string | null, endDate: string | null}>
+
   filterForm: FormGroup = this.formBuilder.group({
-    startDate: [Date],
-    endDate: [Date],
+    startDate: [null],
+    endDate: [null],
   });
   pets: string[] = [];
 
@@ -26,6 +29,34 @@ export class FiltersComponent {
   }
 
   addPetToFilters(pet: string) {
-    this.pets.push(pet);
+    if (!this.pets.includes(pet)) {
+      this.pets.push(pet);
+    } else {
+      this.pets = this.pets.filter(pt => pt !== pet)
+    }
+  }
+
+  onFilterBtnPressed() {
+    let startDateFormated = null;
+    if(this.filterForm.value.startDate) {
+      startDateFormated = this.formatDateToString(this.filterForm.value.startDate);
+    }
+    let endDateFormated = null;
+    if(this.filterForm.value.endDate) {
+      endDateFormated = this.formatDateToString(this.filterForm.value.endDate);
+    }
+
+    this.filters.emit({
+      pets: this.pets,
+      startDate: startDateFormated,
+      endDate: endDateFormated
+    });
+  }
+
+  formatDateToString(date: Date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
   }
 }
